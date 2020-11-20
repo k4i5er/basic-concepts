fb74730291a6ad9f100afa8bffe3888fa1b9d3b5
k4i5er <kaiser@kaiser-hw01.localdomain>
2020-11-20 11:53:12 -0600

    Class Clock implemented

--- basic_concepts.py
+++ basic_concepts.py
@@ -1,146 +1,6 @@
-#  ¿Qué es POO (OOP)?
-#  - Código para realizar funciones
-#  - Trasladar conceptos de la vida cotidiana a programación (abstracción)
-#  - Modelado del mundo cotidiano
-#  - Objetos (características y funcionalidad)
-#    Características: Descripción (son particularidades que describen al objeto)
-#    Funcionalidad: Son las acciones que realizar el objeto por sí mismo (con o sin la intervención de factores externos)
-#  - Clase: Categorización de objetos
-#     Ejemplo: Clase automóvil
-#       Características:
-#         Tipo de motor
-#         Tipo de combustible
-#         Marca
-#         Potencia
-#         Modelo
-#         Tipo
-#         Capacidad de carga
-#         Capacidad de pasajeros
-#         Transmisión
-#         Palanca
-#         Precio
-#         Volante
-#         Diámetro de rin
-#         Tipo de tracción
-#         Tipo de frenos
-#         Tipo de dirección
-#         Condiciones del Chasis
-#         Bolsas de aire
-#         Capacidad de tanque de combustible
-#         Consumo de combustible
-#         Pedales
-#         Faros
-#         Plumas limpia parabrisas
-#         Parabrisas
-#         Ventanillas
-#         Puertas
-#         Asientos
-#         Cinturones de seguridad
-#         Tablero
-#         Bocina
-#     Funcionalidades:
-#         Encender()
-#         Apagar()
-#         Frenar()
-#         Acelerar()
-#         Avanzar()
-#         Retroceder()
-#         Cambiar velocidades()
-#         Girar dirección()
-#         Encender luces()
-#         Apagar luces()
-#         Activar limpia parabrisas()
-#         Desactivar limpia parabrisas()
-#         Activar claxon()
-#         Desactivar claxon
-#         Subir cristales()
-#         Bajar cristales()
-#         Activar alarma()
-#         Desactivar alarma()
-#
-# - Herencia:
-#     mi_coche = automovil()
-#     mi_coche.material_rines = 'aluminio'
-#     otro_coche = automovil()
-#     root = Tk()
-#     root.geometry('100x400')
-#     root.mainloop()
-
 from datetime import datetime, timedelta
-
-# Crear una clase en Python:
-
-
-class Reloj():
-    # Definición de atributos (propiedades)
-    numerals = []
-    mechanism = ''  # Analógico o digital
-    brand = ''
-    type_of = ''  # Pulso, escritorio o pared
-    bell = False
-    led = True
-    hour_hand = False
-    hours = 0
-    minutes = 0
-    seconds = 0
-
-    # Definición de métodos
-    # Getters (métodos que presentan información del estado de los atributos)
-    def get_hour(self):
-        print(f'La hora actual es: {self.hours}:{self.minutes}:{self.seconds}')
-
-    def ring_bell(self):
-        if(self.bell):
-            print('Riiiiiiiiiiing')
-        else:
-            print('Lo siento joven, pero no le vengo manejando las alarmas )=')
-
-    def backlight_on(self):
-        print('Luz encendida')
-
-    def show_clock_items(self):
-        numerals = 0
-        print(f'''
-      Numerales: {self.numerals}
-      Tipo de mecanismo: {self.mechanism}
-      Marca: {self.brand}
-      Tipo de reloj: {self.type_of}
-      Campanilla: {self.bell}
-      Luz interior: {self.led}
-      Manecilla de hora: {self.hour_hand}
-      ''')
-
-    # Setters (métodos que modifican atributos)
-    def set_hour(self, hours, minutes, seconds):
-        self.hours = hours
-        self.minutes = minutes
-        self.seconds = seconds
-
-
-mi_reloj = Reloj()
-mi_reloj.led = True
-mi_reloj.mechanism = 'Digital'
-mi_reloj.type_of = 'Pulso'
-mi_reloj.brand = 'Casio'
-mi_reloj.numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-mi_reloj.hour_hand = False
-mi_reloj.bell = True
-mi_reloj.show_clock_items()
-mi_reloj.ring_bell()
-mi_reloj.set_hour(11, 19, 56)
-mi_reloj.get_hour()
-
-otro_reloj = Reloj()
-otro_reloj.led = False
-otro_reloj.mechanism = 'Analógico'
-otro_reloj.type_of = 'Pared'
-otro_reloj.brand = 'Acme'
-otro_reloj.numerals = ['I', 'II', 'III', 'IV', 'V',
-                       'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']
-otro_reloj.hour_hand = True
-otro_reloj.show_clock_items()
-otro_reloj.ring_bell()
-otro_reloj.get_hour()
+from tkinter import Tk, TOP, X
+from tkinter.ttk import Label, Entry, Button, Frame
 
 # Misión:
 # Crear una clase que permita crear relojes con distintos husos horarios para las siguientes
@@ -163,36 +23,52 @@ class Clock():
     }
 
     # Método constructor
-    def __init__(self, city):
+    def __init__(self, city='México'):
         if not (city) in self.cities:
-            self.city = 'mx'
+            self.city = 'México'
         else:
             self.city = city
 
     def get_hour(self):
         local_time = datetime.now()
-        if self.city == 'mx':
-            print(
-                f'La hora en México es {local_time.hour}:{local_time.minute}:{local_time.second}')
+        if self.city == 'México':
+            return f'La hora en México es {local_time.hour}:{local_time.minute}:{local_time.second}'
         else:
             delta = self.cities[self.city]
             time_zone = timedelta(hours=delta)
             city_time = local_time + time_zone
-            print(
-                f'La hora en {self.city} es {city_time.hour}:{city_time.minute}:{city_time.second}')
+            return f'La hora en {self.city} es {city_time.hour}:{city_time.minute}:{city_time.second}'
+
+    def set_city(self, city_name):
+        self.city = city_name
 
 
-moscu = Clock('Moscú')
-moscu.get_hour()
+my_clock = Clock()
 
-new_york = Clock('Nueva York')
-new_york.get_hour()
 
-tokyo = Clock('Tokio')
-tokyo.get_hour()
+def show_clock():
+    lbl_clock = Label(frm_clock)
+    my_clock.set_city(str(entry_city.get()))  # setter
+    lbl_clock.configure(text=my_clock.get_hour())  # getter
+    lbl_clock.pack(side=TOP)
 
-madrid = Clock('Madrid')
-madrid.get_hour()
 
-mx = Clock('mx')
-mx.get_hour()
+root = Tk()
+root.geometry('400x200')
+root.title('Hora mundial')
+
+frm_city = Frame(root)
+lbl_write_city = Label(
+    root, text="Escribe una ciudad (Madrid, Moscú, México, Nueva York, Tokio)")
+lbl_write_city.pack(side=TOP)
+entry_city = Entry(frm_city)
+entry_city.pack(side=TOP, fill=X, expand=1)
+btn_set_city = Button(text='Ver hora', command=show_clock)
+btn_set_city.pack(side=TOP, after=entry_city)
+frm_city.pack(fill=X)
+
+frm_clock = Frame(root)
+
+frm_clock.pack(fill=X)
+
+root.mainloop()
